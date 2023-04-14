4f932f78431c065bfae28ad7f521d9ecff910e56
mokhtar-dz <81033272+mokhtar-dz@users.noreply.github.com>
2023-04-14 16:38:45 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const API_KEY = '<YOUR_API_KEY>';
+
+const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function sendQueryToChatGPT(prompt) {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [{role: "user", content: prompt}],
+      temperature: 0.6,
+      max_tokens:1000,
+      n: 5
+    });
+    //let reslut = completion.data.choices[0].message.content;
+    let data = completion.data.choices.map((choice) => {
+      return {
+        content: choice.message.content,
+        // add any other properties you need here
+      };
+    });
+    return data;
+  }
+app.use(bodyParser.json());
+
+
+app.post('/correct', async (req, res) => {
+  try {
+    
+
+    const response = await sendQueryToChatGPT("Corriger ce text de tout type d'erreur <"+ req.body.text +">.");
+    re.json({ result:response });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
